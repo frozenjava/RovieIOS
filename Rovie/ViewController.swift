98618d1b5612c46a98d24ad7abc18533ece22246
frozenjava <newbi3@innoc-sec.net>
2015-11-12 22:15:51 -0500

    Added click event to Connect button. Verifying valid IPv4 address

--- Rovie/ViewController.swift
+++ Rovie/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var connectionStatusLabel: UILabel!
+    @IBOutlet weak var hostAddressField: UITextField!
+    @IBOutlet weak var connectHostButton: UIButton!
+    
+    final let IP_REGEX:String! = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+        "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+        "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.connectionStatusLabel.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /**
+     * sendConnectionStatus
+     * Set the connectionStatusLabel text to msg
+     */
+    func sendConnectionStatus(msg:String) {
+        self.connectionStatusLabel.hidden = false
+        self.connectionStatusLabel.text = msg
+    }
+    
+    @IBAction func connectHostButtonAction(sender: UIButton, forEvent event: UIEvent) {
+        let hostAddress:String! = self.hostAddressField.text
+        
+        // Verify a valid IPv4 address
+        if hostAddress.rangeOfString(self.IP_REGEX, options: .RegularExpressionSearch) == nil {
+            self.sendConnectionStatus("Please enter a valid IPv4 address.")
+        } else {
+            self.connectionStatusLabel.hidden = true
+        }
+        
+    }
 
 }
 
