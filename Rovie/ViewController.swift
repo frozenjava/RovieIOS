79c5f3a35b908669902ab8cfc980ff27b294f38d
frozenjava <newbi3@innoc-sec.net>
2015-11-26 21:36:56 -0500

    Connecting to the robot and changing the the controller view now works

--- Rovie/ViewController.swift
+++ Rovie/ViewController.swift
@@ -46,18 +46,47 @@ class ViewController: UIViewController {
     func sendConnectionStatus(msg:String) {
         self.connectionStatusLabel.hidden = false
         self.connectionStatusLabel.text = msg
+        print("-----" + msg)
     }
     
     @IBAction func connectHostButtonAction(sender: UIButton, forEvent event: UIEvent) {
         let hostAddress:String! = self.hostAddressField.text
+        var validAddress:Bool! = false
         
         // Verify a valid IPv4 address
         if hostAddress.rangeOfString(self.IP_REGEX, options: .RegularExpressionSearch) == nil {
             self.sendConnectionStatus("Please enter a valid IPv4 address.")
         } else {
-            self.connectionStatusLabel.hidden = true
+            //self.connectionStatusLabel.hidden = true
+            validAddress = true
         }
         
+        if (validAddress == true) {
+            self.hideKeyboard()
+            let activitiyViewController = ActivityViewController(message: "Connecting...")
+            presentViewController(activitiyViewController, animated: true, completion: nil)
+            // Attempt to connect to the robot
+            HTTPGet("http://" + hostAddress + ":8080/connect") {
+                (data: String, error: String?) -> Void in
+                if error != nil {
+                    print(error)
+                    self.sendConnectionStatus(error!)
+                    print("penis")
+                    self.dismissViewControllerAnimated(true, completion: {})
+                } else {
+                    print("data is : \n\n\n")
+                    print(data)
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                    let rovieController = self.storyboard?.instantiateViewControllerWithIdentifier("Controller") as! RovieControllerViewController
+                    rovieController.setHostAddress(hostAddress)
+                    self.presentViewController(rovieController, animated: true, completion:nil)
+                }
+            }
+            /*if (foundRovie == true) {
+                let rovieController = RovieControllerViewController(host: hostAddress)
+                self.presentViewController(rovieController, animated: true, completion: {})
+            }*/
+        }
     }
 
 }
